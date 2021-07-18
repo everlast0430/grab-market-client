e418983155049b80b4b6c92e4a037c939cbacba3
everlast0430 <sabrikong@gmail.com>
2021-07-18 15:01:51 +0900

    project init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import MainPageComponent from "./main/index.js";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload/index.js";
+import ProductdPage from "./product/index.js";
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path="/">
+          <MainPageComponent />
+        </Route>
+        <Route exact={true} path="/products/:id">
+          <ProductdPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+export default App;
